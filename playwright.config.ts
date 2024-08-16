0d3d1d7903c1fcc74067ddc95dd9b7434907d785
shravankumar637 <shravankumar637@gmail.com>
2024-08-16 13:15:18 +0530

    Creared allure logs

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['allure-playwright', { resultsDir: 'test-results/allure-results' }]
+    ['allure-playwright', { resultsDir: 'allure-results' }]
   ],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
